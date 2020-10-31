9e71640ba0125bb202ddb18d0585670f2a808412
Isabel Aray <limber_10@live.com>
2020-10-31 16:52:30 -0500

    aregloa

--- app/User.php
+++ app/User.php
@@ -4,12 +4,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
+
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens,Notifiable,HasRoles;
+    use Notifiable,HasRoles;
 
     /**
      * The attributes that are mass assignable.
